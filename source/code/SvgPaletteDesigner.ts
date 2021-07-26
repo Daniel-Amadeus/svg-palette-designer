8924a6b77d54015310371a56f9e4eab1090d937e
Daniel-Amadeus Glöckner <dajg1995@gmail.com>
2021-07-26 16:59:05 +0200

    change class

--- source/code/SvgPaletteDesigner.ts
+++ source/code/SvgPaletteDesigner.ts
@@ -16,6 +16,10 @@ export class SvgPaletteDesigner {
         console.log(this._svg);
 
         this.loadSvg(require('../svg/example.svg?raw'));
+
+        const style = document.createElement('style');
+        style.innerHTML = '.magenta{fill: #ff00ff !important}';
+        document.getElementsByTagName('head')[0].appendChild(style);
     }
 
     loadSvg(svg: string): void {
@@ -24,9 +28,24 @@ export class SvgPaletteDesigner {
         const svgPreview = document.getElementById('svgPreview');
         svgPreview.innerHTML = svg;
 
-        const svgElement = svgPreview.children[0] as HTMLElement;
+        const svgElement = svgPreview.children[0] as SVGElement;
         svgElement.style.width = '100%';
         svgElement.style.height = '100%';
 
+        this.addClickListeners(svgElement);
+    }
+
+    addClickListeners(element: Element): void {
+        if (element.children.length == 0) {
+            element.addEventListener('click', (event) => {
+                console.log(element);
+                element.classList.add('magenta');
+            });
+            return;
+        }
+
+        for (const child of element.children) {
+            this.addClickListeners(child);
+        }
     }
 }
